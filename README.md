1d9f5fdebd1a586e05f60710c5dab28d51582ef8
Sebastian <segurasebastian7887@gmail.com>
2019-01-31 13:05:34 -0300

    MongoDB connection and some server setups

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 A Twitter like social network made with Next.js
 
 ## Dependencies
+* **helmet**: helps secure our app by setting various HTTP headers
+* **compression**: gives us gzip compression
+* **morgan**: http request logger middleware for node.js
+* **mongoose**: a mongodb object modeling tool designed to work in an asynchronous environment
+* **mongoose-mongodb-errors**: a plugin to transform mongodb like errors into Mongoose ValidationError instances
+* **passport-local-mongoose**: a Mongoose plugin that simplifies building username and password login with Passport
 
 ## How to install
 
--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const next = require('next');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
 
+// Loads all env variables
 require('dotenv').config();
+
+// Models
+require('./models/user');
+require('./models/post');
 
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3000;
@@ -10,10 +19,45 @@ const ROOT_URL = dev ? `http://localhost:${port}` : process.env.PROD_URL;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+};
+
+mongoose
+  .connect(process.env.MONGO_URI, mongooseOptions)
+  .then(() => console.log('-> DB connected'));
+
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`);
+});
+
 app.prepare().then(() => {
   const server = express();
 
+  // ** only for production **
+  // helmet: helps secure our app by setting various HTTP headers
+  // compression: gives us gzip compression
+  if (!dev) {
+    server.use(helmet());
+    server.use(compression());
+  }
+
+  // morgan for request loggin from client
+  server.use(
+    morgan('dev', {
+      skip: req => req.url.includes('_next')
+    })
+  );
+
   server.use(express.json());
+
+  // Error handling from async / await functions
+  server.use((err, req, res, next) => {
+    const { status = 500, message } = err;
+    res.status(status).json(message);
+  });
 
   // Give all Next.js requests to Next.js server
   server.get('*', (req, res) => {
@@ -22,6 +66,6 @@ app.prepare().then(() => {
 
   server.listen(port, err => {
     if (err) throw err;
-    console.log(`Ready on ${ROOT_URL}`);
+    console.log(`> Ready on ${ROOT_URL}`);
   });
 });
