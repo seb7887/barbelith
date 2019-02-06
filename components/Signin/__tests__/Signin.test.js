c6e0fcfc66c2f48605597eec3ee503833337ee59
Sebastian <segurasebastian7887@gmail.com>
2019-02-06 15:39:09 -0300

    Signin component

---
+++ components/Signin/__tests__/Signin.test.js
@@ -0,0 +1,7 @@
+/* eslint-env jest */
+
+describe('<Signin/>', () => {
+  it('dummy', () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ components/Signup/__tests__/Signup.test.js
@@ -0,0 +1,7 @@
+/* eslint-env jest */
+
+describe('<Signup/>', () => {
+  it('dummy', () => {
+    expect(1).toBe(1);
+  });
+});
